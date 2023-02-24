42a74a7df0a815588db18b089e4a683351e1c55c
Arbaz <arbaz24.3.20@gmail.com>
2023-02-24 13:40:51 +0530

    changes in models

--- models/moviesModel.js
+++ models/moviesModel.js
@@ -1,8 +1,41 @@
 import mongoose from "mongoose";
 
-const movieSchema=new mongoose.Schema({
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+  },
+  img: {
+    type: String,
+  },
+  imgTitle: {
+    type: String,
+  },
+  imgSm: {
+    type: String,
+  },
+  trailer: {
+    type: String,
+  },
+  video: {
+    type: String,
+  },
+  year: {
+    type: String,
+  },
+  limit: {
+    type: Number,
+  },
+  genre: {
+    type: String,
+  },
+  isSeries: {
+    type: Boolean,
+  },
+});
 
-})
-
-const MovieModel =new mongoose.model("Movie",movieSchema);
+const MovieModel = new mongoose.model("Movie", movieSchema);
 export default MovieModel;
